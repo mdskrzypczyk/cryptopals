e0d9b4aa14435c7a37af545e2f93d02ee6b7b3de
mdskrzypczyk <mdskrzypczyk@tuta.io>
2017-06-12 16:28:18 -0500

    Added file for set 3

---
+++ set3.py
@@ -0,0 +1,23 @@
+def challenge17():
+    pass
+
+def challenge18():
+    pass
+
+def challenge19():
+    pass
+
+def challenge20():
+    pass
+
+def challenge21():
+    pass
+
+def challenge22():
+    pass
+
+def challenge23():
+    pass
+
+def challenge24():
+    pass
